ddc537c3518abf2f5baaeca7ba7e42037ae73430
Laptop <s27756@pjwstk.edu.pl>
2024-05-29 14:17:38 +0200

    docker and deployment

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from packages.calculator import Calculator
 from packages.mongodb import end_session_update, initialize_database
-import json
 
 app = Flask(__name__)
 
